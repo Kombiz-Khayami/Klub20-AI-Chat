b937478ee243f0cca418c0d8441235341cfc0f61
Kombiz-Khayami <kombizkhayami@outlook.com>
2023-09-06 20:25:33 +0000

    added env variable for OPENAI_KEY.

--- pages/api/index.js
+++ pages/api/index.js
@@ -14,7 +14,8 @@ const chatRequestOpts = {
   "model": "gpt-3.5-turbo",
   "messages": []
 };
-const assistant = "you are an assistant for a gym that answers questions ONLY about fitness and exercising. if anyone asks you questions not relating to fitness and/or exercising DO NOT answer them. Just say something like \"I'm sorry i'm unable to answer questions like that. But if you havr any fitness or exercising question i'd be happy to answer those.\" "
+const assistant = "you are an assistant for a gym that answers questions ONLY about fitness and exercising. if anyone asks you questions not relating to fitness and/or exercising DO NOT answer them. Just say something like \"I'm sorry i'm unable to answer questions like that. But if you havr any fitness or exercising question i'd be happy to answer those.\"";
+
 
 export default async function handler(req, res) {
 
@@ -29,15 +30,16 @@ export default async function handler(req, res) {
     {role: 'system', content: assistant},
     ...prompt.messages
    ];
-
+   console.log(chatRequestOpts)
   const chatResponse = await fetch('https://api.openai.com/v1/chat/completions', {
     headers: {
-      Authorization: `Bearer ${$OPENAI_KEY}`,
+      Authorization: `Bearer ${process.env.OPENAI_KEY}`,
       'Content-Type': 'application/json'
     },
     method: 'POST',
     body: JSON.stringify(chatRequestOpts)
   }).then((res)=>res.json())
-
+  console.log(process.env.OPENAI_KEY)
+  console.log(chatResponse)
   return res.status(200).json(chatResponse.choices[0].message);
 }
