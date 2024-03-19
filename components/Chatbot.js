fad1dfd0e3605bf3455d23390b8f3a06dd36060f
kombiz khayami <kombizkhayami@outlook.com>
2024-03-19 18:21:04 -0400

    replaced chat bot with survey that gets the client a fitness plan

--- components/Chatbot.js
+++ components/Chatbot.js
@@ -1,16 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation'
-
-import { useChat } from 'ai/react';
-import Link from 'next/link';
+import React, { useState } from 'react';
 
 
 export default function Chatbot() {
-  const router = useRouter();
+
   const [age, setAge] = useState('');
-  const [sex, setSex] = useState('');
+  const [sex, setSex] = useState('male');
   const [bmi, setBMI] = useState('');
-  const [fitnessGoal, setFitnessGoal] = useState('');
+  const [fitnessGoal, setFitnessGoal] = useState('Weight loss');
   const [mainFactors, setMainFactors] = useState(["","","",""])
   const [input, setInput] = useState('')
   let holder = ["","","",""];
@@ -32,10 +28,8 @@ export default function Chatbot() {
     holder = mainFactors;
     holder[3] = "["+event.target.value+"]";
     setMainFactors(holder);
-    setSex(event.target.value);
-    
-     
-    console.log(holder[3]);
+    setSex(event.target.value); 
+    console.log(event.target.value);
   }
 
 
@@ -53,6 +47,7 @@ export default function Chatbot() {
   };
 
   const handleFitnessGoalChange = (event) => {
+    console.log(event.target.value)
     holder = mainFactors;
     holder[0] = "["+event.target.value+"]"
     setMainFactors(holder);
@@ -63,156 +58,80 @@ export default function Chatbot() {
     event.preventDefault()
     holder = mainFactors[0]+","+mainFactors[1]+","+mainFactors[2]+","+mainFactors[3] 
     setInput("Getting Plan...")
-   let fetchPlan = await fetch('api/getPlan', {
-      method: 'POST',
-      body: JSON.stringify({messages: holder})
-   }).then((res)=>res.json());
-   setInput(fetchPlan)
+    let fetchPlan = await fetch('api/getPlan', {
+        method: 'POST',
+        body: JSON.stringify({messages: holder})
+    }).then((res)=>res.json());
+    setInput(fetchPlan)
   }
 
   return (
     <>
-    <form onSubmit={handleSubmit} hidden={input==='' ? false:true}>
-      <div>
-        <label>
-          Age:
-          <input
-            type="text"
-            onChange={handleAgeChange}
-            style={{ borderColor: age === '' ? 'red' : '' }}
-          />
-        </label>
-      </div>
-
-      <div>
-        <label>
-          Sex:
-          <input
-            type="radio"
-            name="Sex"
-            value="Male"            
-            onChange={handleSexChange}
-          />
-          Male
-          <input
-            type="radio"
-            name="Sex"
-            value="Female"            
-            onChange={handleSexChange}
-          />
-          Female
-        </label>
-      </div>
-
-      <div>
-      <label>
-          BMI:
-          <input
-            type="text"
-            onChange={handleBMIChange}
-            style={{ borderColor: bmi === '' ? 'red' : '' }}
-          />
-        </label>
-      </div>
-
-      <div>
-        <label>
-          Fitness Goal:
-          <input
-            type="radio"
-            name="fitnessGoal"
-            value="Weight loss"            
-            onChange={handleFitnessGoalChange}
-          />
-          Weight loss
-          <input
-            type="radio"
-            name="fitnessGoal"
-            value="Build muscle"
-            onChange={handleFitnessGoalChange}
-          />
-          Build muscle
-          <input
-            type="radio"
-            name="fitnessGoal"
-            value="Get toned"
-            onChange={handleFitnessGoalChange}
-          />
-          Get toned
-          <input
-            type="radio"
-            name="fitnessGoal"
-            value="Become stronger"            
-            onChange={handleFitnessGoalChange}
-          />
-          Become stronger
-        </label>
-      </div>
-
-        <button type='submit' disabled = {fitnessGoal === '' || sex === '' || bmi === '' || age === '' ? true : false}>Get Plan</button>
-    </form>
-    <div className="chat-messages" hidden={input != '' ? false:true}>
-      <div className='message assistant' >
-        <p>{input}</p>
-      </div>
-    </div>
-    </>
-  );
-}
-
-/*
-
-import React, { useState, useEffect } from 'react';
-
-import { useChat } from 'ai/react';
-
-export default function Chatbot() {
-
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: "/api",
-  });
-
-  useEffect(() =>{
-    var chatBox = document.getElementById("chatMessages") 
-    chatBox.scrollTo(0,chatBox.scrollHeight)
-    return () =>{}
-  })
-
-  return ( 
-    <>
     <div className='header'>
-      <span><h2>Klub 20</h2></span>
-      <span><h4>Beaconsfield</h4></span> 
+      <span><h2><b>Get your FREE plan for your fitness journey</b></h2></span>
     </div>
 
     <div className="chatbot-header">
-    <span className="chatbot-name" ><h5>Kub20 AI</h5></span>
-
-    <span className="chatbot-description"><em>Let me answer any questions you have about fitness and exercising</em></span>
+      <span className="chatbot-name" ><h3><b>Powered by Kub20 AI</b></h3></span>
     </div>
-    <div className='chatbot-container'>
-      <div className="chatbot">
-        <div className="chat-messages" id="chatMessages">      
-          {messages.map((message, index) => (
-            <div key={index} className={`message ${message.role}`}>
-              <p> {message.role == "user" ? 'User' : 'A.I'} </p>
-              <p>{message.content}</p>
-            </div>
-          ))}
-        </div>
-        <form className="chat-form" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Type a message..."
-            value={input}
-            onChange={handleInputChange}
-          />
-          <button type="submit">Send</button>
+
+    <div className="ai-plan-form-containter">
+      <div class="ai-plan-form">
+        <form onSubmit={handleSubmit} hidden={input==='' ? false:true}>
+          <div>
+            <label>
+              <p>Age:</p>
+              <input
+                type="text"
+                onChange={handleAgeChange}
+                style={{ borderColor: age === '' ? 'red' : '' }}
+              />
+            </label>
+          </div>
+
+          <div>
+            <label>
+              <p>Sex:</p>
+                <select onChange={handleSexChange}>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>            
+            </label>
+          </div>
+
+          <div>
+            <label>
+              <p>BMI:</p>
+                <input
+                  type="text"
+                  onChange={handleBMIChange}
+                  style={{ borderColor: bmi === '' ? 'red' : '' }}
+                />
+            </label>
+          </div>
+
+          <div>
+            <label>
+            <p>Fitness Goal:</p>
+              <select onChange={handleFitnessGoalChange}>
+                <option value="Weight loss">Weight loss</option>
+                <option value="Build muscle">Build muscle</option>
+                <option value="Get toned">Get toned</option>
+                <option value="Become stronger">Become stronger</option>
+              </select>
+            </label>
+          </div>
+
+            <button type='submit' disabled = {fitnessGoal === '' || sex === '' || bmi === '' || age === '' ? true : false}><b>Get Plan</b></button>
         </form>
+        <div className="chat-messages" hidden={input != '' ? false:true}>
+          <div className='message assistant' >
+            <p>{input}</p>
+          </div>
+        </div>
       </div>
     </div>
     </>
   );
 }
 
-*/
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 //import '@/styles/hello.css'
 import '@/styles/index.css'
 //import '@/styles/curio.css'
-
+import '@/styles/aiPlanForm.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
